2674c6481d2b00d5166d2df710ebd87ec0aee729
Gilbert Elnino <gilbeltelnino@gmail.com>
2019-11-26 13:35:12 +0200

    my first test with mocha and chai

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const viewSpecialArticle = require('./routes/special');
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+
 app.use('/api/v1/auth/signup', signup);
 app.use('/api/v1/auth/signin', signin);
 app.use('/api/v1/articles', article);
@@ -20,5 +20,7 @@ app.post('/api/v1/articles/:id/comments', comment);
 app.get('/api/v1/feeds', feeds);
 app.get('/api/v1/articles/:id', viewSpecialArticle);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 6000;
 app.listen(PORT, () => console.log(`app is listerning on port ${PORT}`));
+
+module.exports = app;
